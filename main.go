519446c20fcd992f13e986096b65f4742ca91466
Eduardo Silva <eduardosilva@disroot.org>
2024-12-02 14:07:15 +0000

    Added some signal catching so the program closes more gracefully

--- main.go
+++ main.go
@@ -7,7 +7,9 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/signal"
 	"strings"
+	"syscall"
 	"time"
 )
 
@@ -27,6 +29,16 @@ func msToKph(v float32) int {
 }
 
 func main() {
+	// Set the handlers
+	sigc := make(chan os.Signal, 1)
+	signal.Notify(sigc,
+		syscall.SIGHUP,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		syscall.SIGQUIT,
+	)
+  done := make(chan struct{})
+
 	if len(os.Args) == 3 {
 		fmt.Printf("Port: %s\n", os.Args[1])
 		fmt.Printf("File: %s\n", os.Args[2])
@@ -51,71 +63,88 @@ func main() {
 
 	esdi.Source = &irsdk
 
+	go func() {
+		s := <-sigc
+		switch s {
+		case syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT, syscall.SIGHUP:
+      fmt.Printf("Received signal: %v\n", s)
+			fmt.Print("\033[?25h\033[2J\033[H")
+		}
+    close(done)
+    esdi.Close()
+	}()
+
 	lastTime := time.Now().UnixMilli()
 	lastDataSent := time.Now().UnixMilli()
 	for {
-		time.Sleep(time.Second / 60)
+		select {
+		case <-done:
+			return
+		default:
+			time.Sleep(time.Second / 60)
 
-		var err error
-		var buffer strings.Builder
-		buffer.WriteString("\033[?25l\033[2J\033[H")
+			var err error
+			var buffer strings.Builder
+			buffer.WriteString("\033[?25l\033[2J\033[H")
 
-		err = esdi.Source.UpdateData()
-		if err != nil {
-			fmt.Printf("could not update data: %v", err)
-			continue
-		}
-
-		curGear, err := esdi.Source.GetData("Gear")
-		if err != nil {
-			log.Fatalf("could not get field `Gear`: %v", err)
-		}
-
-		curRPM, err := esdi.Source.GetData("RPM")
-		if err != nil {
-			log.Fatalf("could not get field `RPM`: %v", err)
-		}
-
-		curSpeed, err := esdi.Source.GetData("Speed")
-		if err != nil {
-			log.Fatalf("could not get field `Speed`: %v", err)
-		}
-
-		gear := int32(curGear.(int))
-		rpm := int32(curRPM.(float32))
-		speed := int32(msToKph(curSpeed.(float32)))
-
-		buffer.WriteString(fmt.Sprintf("Gear: %d, RPM: %d, Speed: %d", gear, rpm, speed))
-
-		curTime := time.Now().UnixMilli()
-		message := fmt.Sprintf("%d,%d\n", gear-1, rpm)
-		buffer.WriteString("\n" + message)
-
-		messageWasSentMark := "N"
-		if curTime-lastDataSent > 25 {
-			packet := DataPacket{
-				Speed: speed,
-				Gear:  gear,
-				RPM:   rpm,
-			}
-
-			var buf bytes.Buffer
-			err = binary.Write(&buf, binary.LittleEndian, packet)
-
-			_, err = esdi.SerialConn.Write(buf.Bytes())
+			err = esdi.Source.UpdateData()
 			if err != nil {
-				log.Printf("Unable to write data: %v", err)
-				break
+				fmt.Printf("could not update data: %v", err)
+				continue
 			}
 
-			messageWasSentMark = "Y"
-			lastDataSent = curTime
-		}
+			curGear, err := esdi.Source.GetData("Gear")
+			if err != nil {
+				log.Fatalf("could not get field `Gear`: %v", err)
+			}
 
-		buffer.WriteString(" -> " + messageWasSentMark)
-		if curTime-lastTime > 100 {
-			fmt.Print(buffer.String())
-			lastTime = curTime
+			curRPM, err := esdi.Source.GetData("RPM")
+			if err != nil {
+				log.Fatalf("could not get field `RPM`: %v", err)
+			}
+
+			curSpeed, err := esdi.Source.GetData("Speed")
+			if err != nil {
+				log.Fatalf("could not get field `Speed`: %v", err)
+			}
+
+			gear := int32(curGear.(int))
+			rpm := int32(curRPM.(float32))
+			speed := int32(msToKph(curSpeed.(float32)))
+
+			buffer.WriteString(fmt.Sprintf("Gear: %d, RPM: %d, Speed: %d", gear, rpm, speed))
+
+			curTime := time.Now().UnixMilli()
+			message := fmt.Sprintf("%d,%d\n", gear-1, rpm)
+			buffer.WriteString("\n" + message)
+
+			messageWasSentMark := "N"
+			if curTime-lastDataSent > 25 {
+				packet := DataPacket{
+					Speed: speed,
+					Gear:  gear,
+					RPM:   rpm,
+				}
+
+				var buf bytes.Buffer
+				err = binary.Write(&buf, binary.LittleEndian, packet)
+
+				_, err = esdi.SerialConn.Write(buf.Bytes())
+				if err != nil {
+					log.Printf("Unable to write data: %v", err)
+					break
+				}
+
+				messageWasSentMark = "Y"
+				lastDataSent = curTime
+			}
+
+			buffer.WriteString(" -> " + messageWasSentMark)
+			if curTime-lastTime > 100 {
+				fmt.Print(buffer.String())
+				lastTime = curTime
+			}
+
 		}
 	}
 
