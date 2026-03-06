de3e9784a0364c389a60845b662237a44248e060
Eduardo Silva <eduardosilva@disroot.org>
2026-03-06 19:34:59 +0000

    moved the layout tree to its file

--- cdashdisplay/display.go
+++ cdashdisplay/display.go
@@ -98,28 +98,6 @@ func (s *FString32) UnmarshalYAML(value *yaml.Node) error {
 	return nil
 }
 
-type LayoutTree struct {
-	Windows map[int16]*UIWindow `yaml:"Windows"`
-}
-
-func NewLayoutTree() *LayoutTree {
-	return &LayoutTree{
-		Windows: make(map[int16]*UIWindow),
-	}
-}
-
-func (l *LayoutTree) AddWindow(idx int16, w UIWindow) {
-	pLogger.Debug(fmt.Sprintf("adding window '%d' - %v", idx, w))
-	l.Windows[idx] = &w
-	pLogger.Debug(fmt.Sprintf("new map - %v", l.Windows))
-}
-
-func (l *LayoutTree) RemoveWindow(idx int16) {
-	pLogger.Debug(fmt.Sprintf("removing window '%d'", idx))
-	delete(l.Windows, idx)
-	pLogger.Debug(fmt.Sprintf("new map - %v", l.Windows))
-}
-
 type CDashState struct {
 	Layout *LayoutTree
 }
