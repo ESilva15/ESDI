88b518759c377909f80fbcdd2ab3687525b1ce76
Eduardo Silva <eduardosilva@disroot.org>
2026-03-06 19:33:51 +0000

    deleted useless code

--- tui/internal/models/window.go
+++ tui/internal/models/window.go
@@ -3,19 +3,6 @@ package models
 
 import "esdi/cdashdisplay"
 
-type WindowForm struct {
-	X            uint16
-	Y            uint16
-	Width        uint16
-	Height       uint16
-	TitleSize    uint8
-	TextSize     uint8
-	Title        string
-	Type         uint8
-	ShowID       uint8
-	PreviewValue string
-}
-
 type UIWindow struct {
 	IDX    int16
 	Window cdashdisplay.UIWindow
