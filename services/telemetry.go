bd10d6b4d644103ca77231db78235eca17fe0144
Eduardo Silva <eduardosilva@disroot.org>
2026-09-18 00:07:27 +0100

    subscription fix. should subscribe too fields at once

--- services/telemetry.go
+++ services/telemetry.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"esdi/providers"
+	"esdi/telemetry"
 	telem "esdi/telemetry"
 )
 
@@ -203,15 +204,20 @@ func (t *TelemetryService) UnsubscribeListener(id string) {
 }
 
 func (t *TelemetryService) SubscribeToFields() {
-	// _ = t.devService.SubscribeFields()
-	// TODO: we need to find a way of requesting devices to send all subscribed fields
-	// instead of going through the devices on DevService here
-	// REVAMP
+	seen := make(map[telemetry.FieldID]struct{})
+	var allFields []telemetry.FieldID
+
 	for _, dev := range t.devService.Devices {
-		fields := dev.RequiredFields()
-		t.logger.Debug("requested fields", "fields", fields)
-		t.activeProvider.Subscribe(fields)
+		for _, field := range dev.RequiredFields() {
+			if _, exists := seen[field]; !exists {
+				seen[field] = struct{}{}
+				allFields = append(allFields, field)
+			}
+		}
 	}
+
+	t.logger.Debug("requested fields", "fields", allFields)
+	t.activeProvider.Subscribe(allFields)
 }
 
 func (t *TelemetryService) StartStream() {
